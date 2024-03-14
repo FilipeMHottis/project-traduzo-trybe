000868f4349d5cd0df4660a4f3517c75acc2a98b
Filipe Marques Hottis <lipehottis@gmail.com>
2024-03-14 17:29:59 -0300

    feat: New Controller for render the application

---
+++ src/controllers/translate_controller.py
@@ -0,0 +1,17 @@
+from flask import render_template, Blueprint
+from models.language_model import LanguageModel
+
+
+translate_controller = Blueprint("translate_controller", __name__)
+
+
+@translate_controller.route("/", methods=["GET"])
+def render_languages():
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts() or [],
+        text_to_translate="O que deseja traduzir?",
+        translate_from="pt",
+        translate_to="en",
+        translated="What do you want to translate?",
+    )
--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,19 +1,18 @@
 from models.abstract_model import AbstractModel
-from typing import Dict, List
 from database.db import db
 
 
 class LanguageModel(AbstractModel):
     _collection = db["languages"]
 
-    def __init__(self, data: Dict) -> None:
-        self.data = data
+    def __init__(self, data):
+        super().__init__(data)
 
-    def to_dict(self) -> Dict:
+    def to_dict(self):
         return {"name": self.data["name"], "acronym": self.data["acronym"]}
 
     @classmethod
-    def list_dicts(cls) -> List[Dict]:
+    def list_dicts(cls):
         all_languages = cls._collection.find()
         return [
             {
