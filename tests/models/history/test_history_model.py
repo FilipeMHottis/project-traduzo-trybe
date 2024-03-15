2fd20f2a0d3410d95ba9285102f9892bec479b49
Filipe Marques Hottis <lipehottis@gmail.com>
2024-03-15 12:51:27 -0300

    test: New Test for the History Model

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,34 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
+
+
+expected_answer = [
+    {
+        "text_to_translate": "Hello, I like videogame",
+        "translate_from": "en",
+        "translate_to": "pt",
+    },
+    {
+        "text_to_translate": "Do you love music?",
+        "translate_from": "en",
+        "translate_to": "pt",
+    },
+]
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    historics = json.loads(HistoryModel.list_as_json())
+
+    for index, historic in enumerate(historics):
+        assert (
+            historic["text_to_translate"]
+            == expected_answer[index]["text_to_translate"]
+        )
+        assert (
+            historic["translate_from"]
+            == expected_answer[index]["translate_from"]
+        )
+        assert (
+            historic["translate_to"] == expected_answer[index]["translate_to"]
+        )
