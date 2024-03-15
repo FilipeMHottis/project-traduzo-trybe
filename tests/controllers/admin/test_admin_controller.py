b439178d47019e17519d065d01a227811e7bebd0
Filipe Marques Hottis <lipehottis@gmail.com>
2024-03-15 16:33:35 -0300

    fix: Fix and simplification tests

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,87 +1,69 @@
 from src.models.history_model import HistoryModel
 from src.models.user_model import UserModel
-from src.app import app
-import pytest
 from bson.objectid import ObjectId
+import json
 
 
-history_test = {
-    "text_to_translate": "test",
-    "translate_from": "pt",
-    "translate_to": "en",
-}
-
-user_test = {
-    "name": "test_name",
-    "level": "admin",
-    "token": "test_token",
-}
-
-
-@pytest.fixture(scope="function")
-def client():
-    app.testing = True
-    app_test = app.test_client()
-    yield app_test
-
-    HistoryModel.drop()
-    UserModel.drop()
-
-
-@pytest.fixture(scope="function")
-def user():
-    user = UserModel(user_test)
-    user.save()
-    yield user
-    user.drop()
-
-
-@pytest.fixture(scope="function")
 def history():
-    history = HistoryModel(history_test)
-    history.save()
-    yield history
-    history.drop()
+    HistoryModel(
+        {
+            "text_to_translate": "test",
+            "translate_from": "pt",
+            "translate_to": "en",
+        }
+    ).save()
+
+    return HistoryModel.find_one({"text_to_translate": "test"}).data["_id"]
 
 
-@pytest.fixture(scope="function")
-def object_id():
-    return ObjectId()
+def user():
+    UserModel(
+        {"name": "test_name", "level": "admin", "token": "test_token"}
+    ).save()
+
+    return {"name": "test_name", "level": "admin", "token": "test_token"}
 
 
-def test_history_delete(client, user, history):
-    response = client.delete(
-        f"/admin/history/{history.id}",
+def test_history_delete(app_test):
+    user_test = user()
+    history_id = history()
+
+    response = app_test.delete(
+        f"/admin/history/{history_id}",
         headers={
-            "Authorization": user.data["token"],
-            "User": user.data["name"],
+            "Authorization": user_test["token"],
+            "User": user_test["name"],
         },
     )
 
     assert response.status_code == 204
 
 
-def test_unauthorized_access(client, user, history):
-    response = client.delete(
-        f"/admin/history/{history.id}",
+def test_history_delete_unauthorized(app_test):
+    history_id = history()
+
+    response = app_test.delete(
+        f"/admin/history/{history_id}",
         headers={
             "Authorization": "invalid_token",
-            "User": user.data["name"],
+            "User": "invalid_user",
         },
     )
 
     assert response.status_code == 401
-    assert response.json == {"error": "Unauthorized Access"}
+    assert json.loads(response.data) == {"error": "Unauthorized Access"}
 
 
-def test_history_not_found(client, user, object_id):
-    response = client.delete(
-        f"/admin/history/{object_id}",
+def test_history_not_found(app_test):
+    user_test = user()
+
+    response = app_test.delete(
+        f"/admin/history/{ObjectId()}",
         headers={
-            "Authorization": user.data["token"],
-            "User": user.data["name"],
+            "Authorization": user_test["token"],
+            "User": user_test["name"],
         },
     )
 
     assert response.status_code == 404
-    assert response.json == {"error": "History not found"}
+    assert json.loads(response.data) == {"error": "History not found"}
