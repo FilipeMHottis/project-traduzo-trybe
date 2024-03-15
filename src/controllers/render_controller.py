bb212af139f7258924d7d1b37c075cea176de68f
Filipe Marques Hottis <lipehottis@gmail.com>
2024-03-15 12:15:19 -0300

    feat: Reverse button defined logic

--- src/controllers/render_controller.py
+++ src/controllers/render_controller.py
@@ -13,16 +13,30 @@ class FormRequest(TypedDict):
     translate_to: str
 
 
-@render_controller.route("/", methods=["GET"])
-def index():
+def _translate(source: str, target: str, text: str) -> str:
+    return GoogleTranslator(source=source, target=target).translate(text)
+
+
+def _render(data: FormRequest, translated: str):
     return render_template(
         "index.html",
         languages=LanguageModel.list_dicts(),
-        text_to_translate="O que deseja traduzir?",
-        translate_from="pt",
-        translate_to="en",
-        translated="What do you want to translate?",
+        text_to_translate=data["text_to_translate"],
+        translate_from=data["translate_from"],
+        translate_to=data["translate_to"],
+        translated=translated,
     )
+
+
+@render_controller.route("/", methods=["GET"])
+def index():
+    data: FormRequest = {
+        "text_to_translate": "O que deseja traduzir?",
+        "translate_from": "pt",
+        "translate_to": "en",
+    }
+
+    return _render(data, "What do you want to translate?")
 
 
 @render_controller.route("/", methods=["POST"])
@@ -32,17 +46,27 @@ def translate():
         "translate_from": request.form.get("translate-from"),
         "translate_to": request.form.get("translate-to"),
     }
-    translated = GoogleTranslator(
-        source=data["translate_from"], target=data["translate_to"]
-    ).translate(data["text_to_translate"])
-
-    print(data["translate_from"])
-
-    return render_template(
-        "index.html",
-        languages=LanguageModel.list_dicts(),
-        text_to_translate=data["text_to_translate"],
-        translate_from=data["translate_from"],
-        translate_to=data["translate_to"],
-        translated=translated,
+    translated = _translate(
+        data["translate_from"], data["translate_to"], data["text_to_translate"]
     )
+
+    return _render(data, translated)
+
+
+@render_controller.route("/reverse", methods=["POST"])
+def reverse():
+    data: FormRequest = {
+        "text_to_translate": request.form.get("text-to-translate"),
+        "translate_from": request.form.get("translate-to"),
+        "translate_to": request.form.get("translate-from"),
+    }
+
+    data["text_to_translate"] = _translate(
+        data["translate_to"], data["translate_from"], data["text_to_translate"]
+    )
+
+    translated = _translate(
+        data["translate_from"], data["translate_to"], data["text_to_translate"]
+    )
+
+    return _render(data, translated)
